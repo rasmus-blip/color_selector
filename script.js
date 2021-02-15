8812db43465d28338ea93c80d0d9dbf93e0e47c4
Rasmus Møller <rasm38h8@stud.kea.dk>
2021-02-15 14:36:58 +0100

    did a bit of css, comments and changes in script

--- script.js
+++ script.js
@@ -1,36 +1,29 @@
 "use strict";
 
-window.addEventListener("DOMContentLoaded", sendInput);
+window.addEventListener("DOMContentLoaded", init);
 
-let input = document.querySelector("input");
-let hex;
-
-function sendInput() {
-  input.addEventListener("input", getInput);
+function init() {
+  const input = document.querySelector("input"); // const for the input tag
+  input.addEventListener("input", getInput); // when input tag gets input the getInput function starts
+  getInput(input); // brings input parameter to next function
 }
 
 function getInput(input) {
   input = document.querySelector("input");
-  hex = input.value;
-  console.log(hex);
-
-  document.querySelector("#color").style.backgroundColor = `${hex}`;
-  document.querySelector("#hexcode").textContent = hex;
-  hexToRGB(hex);
+  let hex = input.value; // variable hex becomes equal to the hexvalue of the input
+  document.querySelector("#color").style.backgroundColor = `${hex}`; // changes color of div
+  document.querySelector("#hexcode").textContent = "Hexcode: " + hex; //hex code string is inserted to p
+  hexToRGB(hex); // starts and bring parameters to next function
 }
 
 function hexToRGB(hex) {
-  hex = hex.replace("#", "");
-  let r = parseInt(hex.substring(0, 2), 16);
-  let g = parseInt(hex.substring(2, 4), 16);
-  let b = parseInt(hex.substring(4, 6), 16);
-  let rgb = `rgb(${r}, ${g}, ${b})`;
-  console.log(rgb);
+  let r = parseInt(hex.substring(1, 3), 16); // converts string to number and decides which index from hex to use
+  let g = parseInt(hex.substring(3, 5), 16); // converts string to number and decides which index from hex to use
+  let b = parseInt(hex.substring(5, 7), 16); // converts string to number and decides which index from hex to use
 
-  console.log("rgb(" + r + "," + g + "," + b + ")");
-  document.querySelector("#color2").style.backgroundColor = rgb;
-  document.querySelector("#rgb").textContent = rgb;
-  convertRGBToHSL(r, g, b);
+  const rgbOutput = `RGB: (${r}, ${g}, ${b})`; // writes the rbg values and css
+  document.querySelector("#rgb").textContent = rgbOutput; // insert to html
+  convertRGBToHSL(r, g, b); // starts and bring parameters to next function
 }
 
 function convertRGBToHSL(r, g, b) {
@@ -56,7 +49,6 @@ function convertRGBToHSL(r, g, b) {
   if (h < 0) {
     h = h + 360;
   }
-
   l = (min + max) / 2;
 
   if (max === 0 || min === 1) {
@@ -68,12 +60,7 @@ function convertRGBToHSL(r, g, b) {
   s *= 100;
   l *= 100;
 
-  document.querySelector(
-    "#color3"
-  ).style.backgroundColor = `hsl(${h}, ${s}%, ${l}%)`;
-  document.querySelector("#hsl").textContent = `hsl(${h}, ${s}%, ${l}%)`;
-
-  console.log(`hsl(${h}, ${s}%, ${l}%)`);
+  // amount of decimal is getting shortened to 2 and output is inserset to html
+  let hslOutput = `HSL: (${h.toFixed(2)}, ${s.toFixed(2)}%, ${l.toFixed(2)}%)`;
+  document.querySelector("#hsl").textContent = hslOutput;
 }
-
-function displayInput() {}
